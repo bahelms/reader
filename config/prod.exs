2190dc708e2d8459f995cd9683d33cb3ba007a1b
bahelms <jimbonkgit@gmail.com>
2015-10-10 08:00:29 -0400

    updates prod config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :reader, Reader.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "fathomless-island-3495.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :reader, Reader.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  username: "reader_web",
+  password: "djd*D&djKD9d92j329d",
+  database: "reader_prod",
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
